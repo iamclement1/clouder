9dd054818c38cd4eed15c7b7a5942b7d82703291
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-09-03 08:34:01 +0100

    feat: created the contact us page :hammer:

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Typography from "@/components/common/Typograph";
+import ScreenSize from "@/layouts/ScreenSize";
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      {" "}
+      <Box>
+        <ScreenSize>
+          <Box py="5.27rem">
+            <Typography variant="heading1">
+              <Text textAlign={"center"}>
+                Contact{" "}
+                <Text as="span" color="primary">
+                  {" "}
+                  Us
+                </Text>{" "}
+              </Text>
+            </Typography>
+            <Typography color={"grey_1"} mt="1.87rem" mx={"27rem"}>
+              If you have any question or issues to use our product. Fill the
+              form below. We’ll be glad to help you
+            </Typography>
+          </Box>
+        </ScreenSize>
+      </Box>
+    </div>
+  );
+};
+
+export default page;

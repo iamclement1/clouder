f488d9743b35309acfd9f71cb0b211d6b68da09d
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-09-06 20:09:35 +0100

    feat: configured reducer and persistor :hammer:

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,11 +1,31 @@
 "use client";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import sessionStorage from "redux-persist/lib/storage/session";
+import authReducer from "./feature/auth/authSlice";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistConfig = {
+  key: "persisitRoot",
+  storage: sessionStorage,
+  // blacklist: ["auth"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {},
+  reducer: persistedReducer,
+  middleware: [thunk],
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);

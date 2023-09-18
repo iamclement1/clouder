62cbb0d627635b0b97df9a924949df3cf2032df7
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-09-18 15:29:37 +0100

    feat: configured axios for all my http request :hammer:

---
+++ src/utils/axios.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "https://clouder-lkvb.onrender.com",
+  timeout: 36000,
+});
+
+// Add a request interceptor
+client.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    const token = localStorage.getItem("token");
+
+    if (token !== null) {
+      config.headers.common["Authorization"] = `Bearer ${token}`;
+      config.headers["Content-Type"] = "application/json";
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // delete token from session storage
+    }
+
+    if (error.response.status === 500) {
+      error.response.data.message = "Something went wrong, Please try again!";
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default client;

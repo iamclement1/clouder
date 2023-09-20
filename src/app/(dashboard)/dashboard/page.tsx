feb9672edd2e8306e280520121e4d37f1b88f678
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-09-20 10:34:22 +0100

    feat: link logo back to home, and added content to  dashboard :hammer:

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import { Text } from "@chakra-ui/react";
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Text color={"red"}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero hic deleniti
+      alias nulla sequi, inventore culpa nam modi, quia quisquam, autem ab
+      dolorem tempora magnam! Pariatur aliquid aperiam laborum ut modi, dolor
+      est maiores eveniet quae nisi totam dicta tempore eligendi quaerat
+      excepturi similique temporibus praesentium vitae deleniti doloribus
+      inventore nam molestias doloremque. Asperiores natus odio incidunt
+      corporis vel suscipit sunt eaque, qui similique. Et magni quos, quidem
+      voluptatum explicabo obcaecati dolores! Ut, qui reprehenderit modi
+      distinctio eos minus officia maiores molestias voluptatibus optio,
+      accusantium, unde voluptas voluptate. Eveniet consectetur rem assumenda.
+      Eligendi porro officia ad nesciunt fuga suscipit ab.
+    </Text>
+  );
 };
 
 export default Dashboard;

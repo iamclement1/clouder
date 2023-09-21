db51c6b3707ecbb788239be28c111a75d4aa5474
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-09-21 13:16:56 +0100

    feat: moved the condition to the useeffect hook :weary:

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -1,4 +1,5 @@
-import { useContext } from "react";
+"use client";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "@/context/AuthProvider";
 import { useRouter } from "next/navigation";
 
@@ -6,9 +7,11 @@ const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   // Used AuthContext to access the user
   const { user, userAuthToken } = useContext(AuthContext);
   const router = useRouter();
-  if (!user && !userAuthToken) {
-    router.push("/auth/login"); // Redirect to the login page
-  }
+  useEffect(() => {
+    if (!user && !userAuthToken) {
+      router.push("/auth/login"); // Redirect to the login page
+    }
+  });
 
   console.log(user);
 

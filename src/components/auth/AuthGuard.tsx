aeef5bc87f5de7d42cd87ab43d3551f15b951455
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-09-21 15:28:58 +0100

    refactor: checked readiness of token and user before auth :fire:

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -1,19 +1,41 @@
-"use client";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "@/context/AuthProvider";
 import { useRouter } from "next/navigation";
+import { getCookie } from "cookies-next";
 
 const AuthGuard = ({ children }: { children: React.ReactNode }) => {
-  // Used AuthContext to access the user
   const { user, userAuthToken } = useContext(AuthContext);
   const router = useRouter();
-  useEffect(() => {
-    if (!user && !userAuthToken) {
-      router.push("/auth/login"); // Redirect to the login page
-    }
-  });
+  const [isReady, setIsReady] = useState(false); // Add a state to track readiness
 
-  console.log(user);
+  useEffect(() => {
+    if (!isReady) {
+      // If not ready, return without redirection
+      return;
+    }
+
+    if (!user || !userAuthToken) {
+      router.push("/auth/login");
+    }
+  }, [isReady, user, userAuthToken, router]);
+
+  useEffect(() => {
+    // Check for user data in sessionStorage
+    const checkAuth = () => {
+      if (typeof window !== "undefined") {
+        const cookies = getCookie("token");
+        const user = sessionStorage.getItem("user");
+        if (!user && !cookies) {
+          router.push("/auth/login");
+        }
+        return setIsReady(true); // Set readiness when user data is available
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  // console.log(user, userAuthToken);
 
   return children;
 };

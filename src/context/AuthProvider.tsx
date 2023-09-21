86c32114f619e916d6f35be182a22b43dc8c2b92
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-09-21 15:30:44 +0100

    refactor: moved login mutate func here to initiate user :hammer:

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,4 +1,8 @@
-import { getCookie } from "cookies-next";
+import { FormValues } from "@/components/auth/Login";
+import api from "@/utils/axiosInstance";
+import { useMutation } from "@tanstack/react-query";
+import { getCookie, setCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 import React, {
   createContext,
   useContext,
@@ -9,13 +13,18 @@ import React, {
 
 type authContextType = {
   user: boolean;
+  login: (user: FormValues) => void;
   userAuthToken: string;
+  loading: boolean;
 };
 
-//TODO: check why there is always a tree mismatch
+//TODO: check why there is always a tree mismatch --> This was resolved by calling the login function here
+//TODO: I need to confirm after the above is resolved on browser reload the page returns to login -->resolved in the authguard by checking readiness
 const authContextDefaultValues: authContextType = {
   user: false,
+  login: () => {},
   userAuthToken: "",
+  loading: false,
 };
 
 export const AuthContext = createContext<authContextType>(
@@ -33,23 +42,49 @@ type Props = {
 export function AuthProvider({ children }: Props) {
   const [user, setUser] = useState<boolean>(false);
   const [userAuthToken, setUserAuthToken] = useState<string>("");
-  console.log(userAuthToken);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  //function to check auth state
-  const checkAuth = () => {
-    if (typeof window !== "undefined") {
-      if (!user && !userAuthToken) {
-        const cookies = getCookie("token");
-        const user = sessionStorage.getItem("user");
-        if (user && cookies) {
-          setUserAuthToken(cookies);
+  const router = useRouter();
+
+  const { mutate, isLoading } = useMutation({
+    mutationFn: async (user: FormValues) => {
+      try {
+        setLoading(isLoading);
+        const response = await api.post("/auth/signin", user);
+        if (response.status === 201) {
+          const userData = JSON.stringify(response.data);
+          const userToken = response.data.access;
+          const refreshToken = response.data.refresh;
+          sessionStorage.setItem("user", userData);
+          setCookie("token", userToken);
+          setCookie("refresh", refreshToken);
+          setUserAuthToken(userToken);
           setUser(true);
+          router.push("/dashboard");
         }
+        setLoading(!isLoading);
+      } catch (error) {
+        console.error("Login failed:", error);
+        throw error; // Re-throw the error to propagate it to the caller
       }
-    }
-  };
+    },
+  });
 
   useEffect(() => {
+    //function to check auth state
+    const checkAuth = () => {
+      if (typeof window !== "undefined") {
+        if (!user && !userAuthToken) {
+          const cookies = getCookie("token");
+          const user = sessionStorage.getItem("user");
+          if (user && cookies) {
+            setUserAuthToken(cookies);
+            setUser(true);
+          }
+        }
+      }
+    };
+
     checkAuth();
   }, []);
 
@@ -58,6 +93,8 @@ export function AuthProvider({ children }: Props) {
     setUser,
     userAuthToken,
     setUserAuthToken,
+    login: mutate,
+    loading,
   };
 
   return (

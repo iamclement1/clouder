176e8bd7d331f9a776fd77754f1937bf7dca6911
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-09-25 12:46:26 +0100

    revert: revert to initial configuration :hammer:

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,7 +1,3 @@
-import api from "@/utils/axiosInstance";
-import { LoginFormValues } from "@/utils/types";
-import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
 import React, {
   createContext,
   useContext,
@@ -13,18 +9,14 @@ import React, {
 
 type authContextType = {
   user: boolean;
-  login: (user: LoginFormValues) => void;
   userAuthToken: string;
-  loading: boolean;
 };
 
 //TODO: check why there is always a tree mismatch --> This was resolved by calling the login function here
 //TODO: I need to confirm after the above is resolved on browser reload the page returns to login -->resolved in the authguard by checking readiness
 const authContextDefaultValues: authContextType = {
   user: false,
-  login: () => {},
   userAuthToken: "",
-  loading: false,
 };
 
 export const AuthContext = createContext<authContextType>(
@@ -42,33 +34,6 @@ type Props = {
 export function AuthProvider({ children }: Props) {
   const [user, setUser] = useState<boolean>(false);
   const [userAuthToken, setUserAuthToken] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const router = useRouter();
-
-  const { mutate, isLoading } = useMutation({
-    mutationFn: async (user: LoginFormValues) => {
-      try {
-        setLoading(isLoading);
-        const response = await api.post("/auth/signin", user);
-        if (response.status === 201) {
-          const userData = JSON.stringify(response.data);
-          const userToken = response.data.access;
-          const refreshToken = response.data.refresh;
-          sessionStorage.setItem("user", userData);
-          sessionStorage.setItem("token", userToken);
-          sessionStorage.setItem("refreshToken", refreshToken);
-          setUserAuthToken(userToken);
-          setUser(true);
-          router.push("/dashboard");
-        }
-        setLoading(!isLoading);
-      } catch (error) {
-        console.error("Login failed:", error);
-        throw error; // Re-throw the error to propagate it to the caller
-      }
-    },
-  });
 
   useEffect(() => {
     //function to check auth state
@@ -94,10 +59,8 @@ export function AuthProvider({ children }: Props) {
       setUser,
       userAuthToken,
       setUserAuthToken,
-      login: mutate,
-      loading,
     }),
-    [user, setUser, userAuthToken, setUserAuthToken, mutate, loading],
+    [user, setUser, userAuthToken, setUserAuthToken],
   );
 
   return (

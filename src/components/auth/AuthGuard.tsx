c9057a07e7e7e745b9cbe3d0a28892699d0c27de
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-10-31 13:47:11 +0100

    refactor: auth guard :fire:

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -1,18 +1,13 @@
-import { useContext, useEffect } from "react";
+import { getStorageAuthItems } from "@/utils/lib";
 import { redirect } from "next/navigation";
-import { AuthContext } from "@/context/AuthProvider";
 
 const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   //this line extracts the user and userAuthToken properties from the authContext
-  const { user, userAuthToken } = useContext(AuthContext);
 
-  useEffect(() => {
-    if (!user || !userAuthToken) {
-      //if none of the following is true, return user to login page
-      redirect("/auth/login");
-    }
-  }, [user, userAuthToken, redirect]);
+  const userAuthToken = getStorageAuthItems();
 
+  if (!userAuthToken.token) return redirect("/auth/login");
+  console.log(userAuthToken.token);
   return children;
 };
 

82b943f2b3c33f9d783687f75f318071426f8ab8
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-11-11 08:10:14 +0100

    fix: removed the dob field in update user :tada:

--- src/components/dashboard/profile/UpdateProfile.tsx
+++ src/components/dashboard/profile/UpdateProfile.tsx
@@ -6,21 +6,27 @@ import useProfile from "@/hooks/useProfile";
 import api from "@/utils/axiosInstance";
 import { ProfileFormValues } from "@/utils/types";
 import { Box, Flex, Image, Text, Stack } from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
+import { Form, Formik } from "formik";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import { toast } from "react-toastify";
 
 const UpdateProfile: React.FC = () => {
   const { data, isLoading } = useProfile();
+  const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
   const [uploadedImage, setUploadedImage] = React.useState<File | null>(null);
+  console.log(data);
 
   if (isLoading) return <p>Fetching your information, please wait....</p>;
 
   const userData = data?.data;
   // function to update user profile
 
+  const handleImageSave = () => {
+    // Handle image save logic if needed
+    console.log("Image saved!");
+  };
   return (
     <Box>
       <Flex justify="center">
@@ -37,7 +43,11 @@ const UpdateProfile: React.FC = () => {
             Please upload image
           </Text>
 
-          <UploadImage onUpload={(file) => setUploadedImage(file)} />
+          <UploadImage
+            onUpload={(file) => setUploadedImage(file)}
+            onSave={handleImageSave}
+            uploadedImage={uploadedImage}
+          />
 
           <Text fontSize="1.3125rem" fontWeight="400" mt="2rem">
             Welcome back {userData?.fullName} 😊
@@ -54,7 +64,6 @@ const UpdateProfile: React.FC = () => {
           initialValues={{
             fullName: userData?.fullName ?? "",
             email: userData?.email ?? "",
-            dob: userData?.dob ?? "",
             phone: userData?.phone ?? "",
           }}
           validate={(values: ProfileFormValues) => {
@@ -65,9 +74,6 @@ const UpdateProfile: React.FC = () => {
             if (!values.email) {
               errors.email = "Required";
             }
-            // if (!values.dob) {
-            //     errors.dob = "Required";
-            // }
             if (!values.phone) {
               errors.phone = "Required";
             }
@@ -76,26 +82,42 @@ const UpdateProfile: React.FC = () => {
           }}
           onSubmit={async (values: ProfileFormValues) => {
             const formData = new FormData();
-            formData.append("image", uploadedImage as Blob);
-            formData.append("fullName", values.fullName);
-            formData.append("email", values.email);
-            formData.append("dob", values.dob);
-            formData.append("phone", values.phone);
-            console.log({ formData });
+
+            switch (true) {
+              case !!uploadedImage:
+                formData.append("image", uploadedImage as Blob);
+                break;
+              case values.fullName !== userData?.fullName:
+                formData.append("fullName", values.fullName);
+                break;
+              case values.email !== userData?.email:
+                formData.append("email", values.email);
+                break;
+              case values.phone !== userData?.phone:
+                formData.append("phone", values.phone);
+                break;
+              default:
+                break;
+            }
+
             try {
+              setLoading(true);
               const response = await api.patch("/user/update", formData, {
                 headers: {
                   "Content-Type": "multipart/form-data",
                 },
               });
+
               if (response.status === 200) {
+                setLoading(false);
                 const msg = response?.data.message;
                 toast.success(msg);
                 router.push("/dashboard");
               }
-              // Handle the response as needed
+
               console.log(response);
             } catch (error) {
+              setLoading(false);
               console.error("Error uploading data:", error);
             }
           }}
@@ -130,39 +152,6 @@ const UpdateProfile: React.FC = () => {
                   justify="center"
                   gap={["12px", "2rem"]}
                 >
-                  {/* DOB section */}
-                  <Box w={["100%"]} mt="12px">
-                    <label
-                      style={{
-                        display: "block",
-                        fontSize: "0.84375rem",
-                        margin: "0px 12px 8px 0px",
-                      }}
-                      htmlFor="dob"
-                    >
-                      Date of Birth
-                    </label>
-                    <Field
-                      // {...props}
-                      // as={Field}
-                      id={"dob"}
-                      name={"dob"}
-                      type="date"
-                      disabled
-                      placeholder={"date of birth"}
-                      style={{
-                        fontSize: "0.75rem",
-                        padding: "12px 20px",
-                        display: "block",
-                        border: "1px solid #ACB5BD",
-                        width: "100%",
-                        height: "3.23438rem",
-                        color: "#353945",
-                        borderRadius: "5px",
-                      }}
-                    />
-                  </Box>
-
                   {/* Phone number */}
                   <CustomInput
                     label="Phone number"
@@ -191,7 +180,7 @@ const UpdateProfile: React.FC = () => {
                     mt="1.59rem"
                     h="3.2rem"
                     w="100%"
-                    isLoading={isLoading}
+                    isLoading={loading}
                     // isDisabled={isAccept}
                   >
                     Update

e01065ceabce0adb44a50ffb07f60b04fe375f36
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-11-26 23:03:59 +0100

    feat: logout:tada:

--- src/app/dashboard/logout/page.tsx
+++ src/app/dashboard/logout/page.tsx
@@ -3,7 +3,11 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 
 const Logout = () => {
-  return <Box>Logout</Box>;
+  return (
+    <Box>
+      <div>Logout</div>
+    </Box>
+  );
 };
 
 export default Logout;

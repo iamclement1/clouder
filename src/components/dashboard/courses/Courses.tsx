57cf7a9b3d5097f39914956c4740c91de4cb6739
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-12-16 10:00:30 +0100

    refactor: reverse before mapping :bulb:

--- src/components/dashboard/courses/Courses.tsx
+++ src/components/dashboard/courses/Courses.tsx
@@ -69,45 +69,48 @@ const Courses = () => {
                     </Flex>
 
                     <OrderedList mt="2.2rem">
-                      {courses?.map((item) => {
-                        return (
-                          <ListItem
-                            mb={"1rem"}
-                            color="grey_1"
-                            key={item.school}
-                            fontSize="1.125rem"
-                            fontWeight="600"
-                            display="flex"
-                            alignItems="center"
-                            justifyContent={"space-between"}
-                          >
-                            <Text
-                              onClick={() =>
-                                router.push(
-                                  `/dashboard/courses/course_aquired/${item?.id}`,
-                                )
-                              }
-                              cursor={"pointer"}
-                            >{`${item.institution}
+                      {courses
+                        ?.slice()
+                        .reverse()
+                        .map((item) => {
+                          return (
+                            <ListItem
+                              mb={"1rem"}
+                              color="grey_1"
+                              key={item?.id}
+                              fontSize="1.125rem"
+                              fontWeight="600"
+                              display="flex"
+                              alignItems="center"
+                              justifyContent={"space-between"}
+                            >
+                              <Text
+                                onClick={() =>
+                                  router.push(
+                                    `/dashboard/courses/course_aquired/${item?.id}`,
+                                  )
+                                }
+                                cursor={"pointer"}
+                              >{`${item?.institution}
                                                             `}</Text>
 
-                            <Text
-                              bgColor="danger_2"
-                              fontSize="0.75rem"
-                              color="danger_1"
-                              fontWeight="normal"
-                              w="fit-content"
-                              p="0.8rem 1rem"
-                              rounded={"1.35938rem"}
-                              cursor="pointer"
-                              as="a"
-                              href={`/dashboard/courses/request_feed_back/${item.year}`}
-                            >
-                              Request feedback
-                            </Text>
-                          </ListItem>
-                        );
-                      })}
+                              <Text
+                                bgColor="danger_2"
+                                fontSize="0.75rem"
+                                color="danger_1"
+                                fontWeight="normal"
+                                w="fit-content"
+                                p="0.8rem 1rem"
+                                rounded={"1.35938rem"}
+                                cursor="pointer"
+                                as="a"
+                                href={`/dashboard/courses/request_feed_back/${item?.year}`}
+                              >
+                                Request feedback
+                              </Text>
+                            </ListItem>
+                          );
+                        })}
                     </OrderedList>
                   </Box>
                 ) : (

3171117f8c25cabf259a7a56b24c05261f708ee1
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-01-06 23:12:32 +0100

    feat: mutate handle logout :fire:

---
+++ src/hooks/useSignOut.tsx
@@ -0,0 +1,39 @@
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import api from "@/utils/axiosInstance";
+
+const useSignOut = () => {
+  const router = useRouter();
+
+  const { mutate, isLoading } = useMutation({
+    mutationFn: () => {
+      return api.get("/user/signout");
+    },
+    onSuccess: ({ data }) => {
+      sessionStorage.removeItem("token");
+      sessionStorage.removeItem("refreshToken");
+      toast.success(data.message, {
+        theme: "dark",
+      });
+      router.push("/");
+    },
+    onError: (error: { response: { data: { error: string } } }) => {
+      const errorMsg = error.response.data.error;
+      toast.error(errorMsg, {
+        theme: "dark",
+      });
+    },
+  });
+
+  const handleLogOut = () => {
+    mutate();
+  };
+
+  return {
+    isLoading,
+    handleLogOut,
+  };
+};
+
+export default useSignOut;

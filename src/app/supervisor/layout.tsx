be6ce18aec9c2e600d11e98b81d8a3212c1225be
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-01-16 07:59:10 +0100

    feat: check user role :bulb:

--- src/app/supervisor/layout.tsx
+++ src/app/supervisor/layout.tsx
@@ -7,17 +7,31 @@ import InactivityCheck from "@/components/common/IdleCheckModal";
 import { getStorageAuthItems } from "@/utils/lib";
 import { redirect } from "next/navigation";
 import SupervisorSidebarWithHeader from "@/components/common/SupervisorSidebar";
+import useProfile from "@/hooks/useProfile";
+import { toast } from "react-toastify";
+import PageLoader from "@/components/common/PageLoader";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data: profileData, isLoading } = useProfile();
+  if (isLoading) return <PageLoader />;
+  console.log(profileData);
+  const userRole = profileData?.data;
   useEffect(() => {
     const { token } = getStorageAuthItems();
+    console.log("Token:", token);
+    console.log("User Role:", userRole);
 
-    if (!token) return redirect("/auth/login");
-  }, []);
+    if (!token || userRole?.role !== "supervisor") {
+      // Redirect to login
+      toast.error("Unauthorized, retry with authorized credentials");
+      return redirect("/auth/login");
+    }
+  }, [profileData]);
+
   return (
     <React.Fragment>
       <Seo templateTitle="Clouder" />

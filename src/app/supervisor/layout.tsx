3ec8c82f109e0cb1331a46c658707f7a7f2db134
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-07-30 10:46:15 +0100

    Fix/dynamic routes (#188)
    * Chore/update-user-routes (#185) (#186)
    
    * chore: filter routes based on user plans :green_heart:
    
    * chore: include user profile to constant routes :tada:
    
    * refactor: middleware :hammer:
    
    * chore: req.body update :green_heart:
    
    * refactor: courses component :hammer:
    
    * refactor: input fields :hammer:
    
    * chore: include dynamic route :green_heart:
    
    * chore: check request props :tada:
    
    * feat: use submission for all modules :green_heart:
    
    * feat: mutation for feedback :tada:
    
    * chore: update :tada:
    
    * chore: remove type error :tada:
    
    * chore: remove type error :tada:
    
    * chore: remove type error :tada:
    
    * chore: update dynamic routes :tada
    
    * chore: update dynamic routes :tada:
    
    * chore: update dynamic routes :tada:
    
    * refactor: middleware :green_heart
    
    * chore: include metadata :sprinkles:
    
    * chore: include pricing :sprinkles:

--- src/app/supervisor/layout.tsx
+++ src/app/supervisor/layout.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import BackToTop from "@/components/common/BackToTop";
-import Seo from "@/components/common/SEO";
 import InactivityCheck from "@/components/common/IdleCheckModal";
 import SupervisorSidebarWithHeader from "@/components/common/SupervisorSidebar";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Clouder",
+    default: "Supervisor || Clouder",
+  },
+  description: "A medical portfolio",
+  manifest: "/manifest.json",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      url: "./icon.png",
+    },
+    {
+      rel: "apple",
+      url: "./icon.png",
+    },
+  ],
+};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -11,8 +30,6 @@ export default function RootLayout({
 }>) {
   return (
     <React.Fragment>
-      <Seo templateTitle="Clouder" />
-
       <SupervisorSidebarWithHeader passedActive="/supervisor">
         <InactivityCheck />
 
